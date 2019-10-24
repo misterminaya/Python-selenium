fd95f79b8dd8789a621a25d8d0c8df631e91c17e
misterminaya <misterminayape@gmail.com>
2019-10-23 23:57:55 -0500

    page object parte 3 -refactory

--- Pages/FlightPage.py
+++ Pages/FlightPage.py
@@ -1,27 +1,30 @@
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 import unittest
+
+
 
 class FlightPage:
     def __init__(self,myDriver):
         self.driver = myDriver
-        self.countryDropDown = Select(self.driver.find_element_by_name("country"))
+        self.countryDropDown = (By.NAME, 'country')
 
     def select_country_by_index(self, index):
-        self.countryDropDown.select_by_index(index)
+        Select(self.driver.find_element(*self.countryDropDown)).select_by_index(index)
 
     def select_country_by_value(self, value):
-        self.countryDropDown.select_by_value(value)
+        Select(self.driver.find_element(*self.countryDropDown)).select_by_value(value)
 
-    def select_country_bv_name(self, name):
-        self.countryDropDown.select_by_visible_text(name)
+    def select_country_by_name(self, name):
+        Select(self.driver.find_element(*self.countryDropDown)).select_by_visible_text(name)
 
     def verify_country(self, country):
         tc = unittest.TestCase('__init__')
-        tc.assertEqual(self.countryDropDown.first_selected_option.text.strip(), country)
+        tc.assertEqual(Select(self.find_element(*self.countryDropDown)).first_selected_option.text.strip(), country)
 
     def verify_not_country(self, country):
         tc = unittest.TestCase('__init__')
-        tc.assertNotEqual(self.countryDropDown.first_selected_option.text.strip(), country)
+        tc.assertNotEqual(Select(self.find_element(*self.countryDropDown)).first_selected_option.text.strip(), country)
 
     def verify_true_country(self, country):
         tc = unittest.TestCase('__init__')
